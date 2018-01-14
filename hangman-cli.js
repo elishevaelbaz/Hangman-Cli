1c8cc940e2699426dc70b8efd6f0f446118820a3
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2018-01-13 19:29:17 -0500

    added some comments

--- hangman-cli.js
+++ hangman-cli.js
@@ -1,4 +1,7 @@
 
+// the repo for this game should be linked in my portfolio
+// pages link for portfolio: https://elishevaelbaz.github.io/
+
 var Word = require("./word.js");
 
 var inquirer = require("inquirer");
